15d91abaca02e7dd051c90eaa7dc494202a3ea14
Anirudh1612 <apsharma113@gmail.com>
2023-11-20 08:00:39 +0530

    Update keep-alive.js

--- keep-alive.js
+++ keep-alive.js
@@ -4,7 +4,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }
-).listen(1337, '127.0.0.1');
+).listen(1337);
 console.log('Server running at XXXXXXXXXXXXXXXXXXXXXX');
 
 
