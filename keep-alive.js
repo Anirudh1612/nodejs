7d60a2009fdc8ea28ba56bd595bd0550973e75c4
Anirudh1612 <apsharma113@gmail.com>
2023-11-20 07:56:43 +0530

    keep-alive-basic

---
+++ keep-alive.js
@@ -0,0 +1,10 @@
+//Write a sample javascript hello world code with connection type set to keep-alive
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}
+).listen(1337, '127.0.0.1');
+console.log('Server running at XXXXXXXXXXXXXXXXXXXXXX');
+
+
